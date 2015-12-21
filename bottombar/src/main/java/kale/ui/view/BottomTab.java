bf3ea362f266f375a6c849fe67666470ebfd9779
Devin.Fu <devin5719@gmail.com>
2015-12-21 11:21:40 +0800

    整理代码

--- bottombar/src/main/java/kale/ui/view/BottomTab.java
+++ bottombar/src/main/java/kale/ui/view/BottomTab.java
@@ -5,46 +5,18 @@ import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.widget.CompoundButton;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import kale.kale.bottomtab.BottomTabImpl;
 import kale.bottombar.R;
+import kale.kale.bottomtab.BottomTabImpl;
 
-/**
- * @author Jack Tony
- * @date 2015/6/7
- */
 public class BottomTab extends LinearLayout implements BottomTabImpl {
 
-    public BottomTab(Context context) {
-        this(context, null);
-    }
-
-    public BottomTab(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        if (attrs == null) {
-            // Preserve behavior prior to removal of this API.
-            throw new NullPointerException();
-        }
-        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BottomTab);
-        tabTextColors = a.getColorStateList(R.styleable.BottomTab_android_textColor);
-        tabText = a.getText(R.styleable.BottomTab_android_text);
-        tabTextSize = a.getDimensionPixelSize(R.styleable.BottomTab_android_textSize, 16);
-        tabDrawable = a.getDrawable(R.styleable.BottomTab_android_drawableTop);
-        a.recycle();
-        initViews();
-    }
-
-    CharSequence tabText;
-    int tabTextSize;
-
-    ColorStateList tabTextColors;
-
-    Drawable tabDrawable;
+    private CharSequence mTabTitle;
+    private int mTabTitleSize;
+    private ColorStateList mTabTextColors;
+    private Drawable mTabDrawable;
 
     private TextView mTvTabHint;
     private RadioImageView mRivTabIcon;
@@ -54,22 +26,39 @@ public class BottomTab extends LinearLayout implements BottomTabImpl {
 
     private OnCheckedChangeListener mOnCheckedChangeListener;
 
+    public BottomTab(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        if (attrs == null) {
+            // Preserve behavior prior to removal of this API.
+            throw new NullPointerException();
+        }
+
+        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BottomTab);
+        mTabTextColors = a.getColorStateList(R.styleable.BottomTab_android_textColor);
+        mTabTitle = a.getText(R.styleable.BottomTab_android_text);
+        mTabTitleSize = a.getDimensionPixelSize(R.styleable.BottomTab_android_textSize, 16);
+        mTabDrawable = a.getDrawable(R.styleable.BottomTab_android_drawableTop);
+        a.recycle();
+
+        this.initViews();
+    }
+
     private void initViews() {
-        this.setOrientation(VERTICAL);
+        super.setOrientation(VERTICAL);
         inflate(getContext(), getLayoutRes(), this);
+
         mTvTabHint = (TextView) findViewById(R.id.tab_hint);
         mRivTabIcon = (RadioImageView) findViewById(R.id.tab_icon);
         mTvTabTitle = (RadioTextView) findViewById(R.id.tab_title);
 
-        mRivTabIcon.setImageDrawable(tabDrawable);
-        mTvTabTitle.setText(tabText);
-        mTvTabTitle.setTextSize(tabTextSize);
-        if (tabTextColors != null) {
-            mTvTabTitle.setTextColor(tabTextColors);
+        mRivTabIcon.setImageDrawable(mTabDrawable);
+        mTvTabTitle.setText(mTabTitle);
+        mTvTabTitle.setTextSize(mTabTitleSize);
+        if (mTabTextColors != null) {
+            mTvTabTitle.setTextColor(mTabTextColors);
         }
 
-        mRivTabIcon.setOnCheckedChangeListener(new RadioImageView.OnCheckedChangeListener()
-        {
+        mRivTabIcon.setOnCheckedChangeListener(new RadioImageView.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioImageView pView, boolean isChecked) {
                 mChecked = isChecked;
@@ -83,7 +72,7 @@ public class BottomTab extends LinearLayout implements BottomTabImpl {
         });
     }
 
-    
+
     public int getLayoutRes() {
         return R.layout.bottom_tab_main;
     }
